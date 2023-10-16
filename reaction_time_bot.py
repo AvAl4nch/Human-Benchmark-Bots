e60a0e4b59173ec307422a220d078e98068b7efd
AvAl4nch <129538666+AvAl4nch@users.noreply.github.com>
2023-10-16 18:55:29 +0300

    Update reaction_time_bot.py

--- reaction_time_bot.py
+++ reaction_time_bot.py
@@ -1,14 +1,38 @@
 import pyautogui
 import time
+import argparse
 
-time.sleep(3)
-c = 0
 
-while True:
-    x, y = 300, 600
-    pixel = pyautogui.pixel(300, 600)
-    if pixel == (75, 219, 106): # the RGB values for the  green color 
-        pyautogui.click(x, y)
-        pyautogui.click(x, y)
-        c = c + 1
-        if c == 5: break
+def main():
+    args = parse_arguments()
+
+    delay = args.delay
+    green = args.green
+
+    if green:
+        r, g, b = green
+    else:
+        r, g, b = 75, 219, 106
+
+    time.sleep(delay)
+    c = 0
+
+    while True:
+        x, y = 300, 600
+        pixel = pyautogui.pixel(300, 600)
+        if pixel == (r, g, b):  # the RGB values for the  green color
+            pyautogui.click(x, y)
+            pyautogui.click(x, y)
+            c = c + 1
+            if c == 5: break
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Typing Speed Bot')
+    parser.add_argument('--delay', type=float, default=3, help='Delay before starting')
+    parser.add_argument('--green', nargs=3, type=int, help='Green RGB values R G B')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
