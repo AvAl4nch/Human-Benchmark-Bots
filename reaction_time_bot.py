ec358692bee3ec69eca3ac0720f5199d4e1b928f
AvAl4nch <129538666+AvAl4nch@users.noreply.github.com>
2023-10-16 18:45:02 +0300

    Update reaction_time_bot.py

--- reaction_time_bot.py
+++ reaction_time_bot.py
@@ -5,10 +5,10 @@ time.sleep(3)
 c = 0
 
 while True:
-
+    x, y = 300, 600
     pixel = pyautogui.pixel(300, 600)
-    if pixel == (75, 219, 106):
-        pyautogui.click(100, 600)
-        pyautogui.click(100, 600)
+    if pixel == (75, 219, 106): # the RGB values for the  green color 
+        pyautogui.click(x, y)
+        pyautogui.click(x, y)
         c = c + 1
         if c == 5: break
